d4da08eb87aa15af603b31c31f56985d13025296
Inácio Mattos <inaciotm@hotmail.com>
2019-01-18 01:28:50 -0200

    Meus projetos

---
+++ static/javascript/home.js
@@ -0,0 +1,65 @@
+function proj_enter1()
+{
+	var proj = document.querySelector(".info-projeto1");
+	proj.classList.toggle("ativo");
+}
+function proj_out1()
+{
+	var proj = document.querySelector(".info-projeto1");
+	proj.classList.toggle("ativo");
+}
+
+function proj_enter2()
+{
+	var proj = document.querySelector(".info-projeto2");
+	proj.classList.toggle("ativo");
+}
+function proj_out2()
+{
+	var proj = document.querySelector(".info-projeto2");
+	proj.classList.toggle("ativo");
+}
+
+function proj_enter3()
+{
+	var proj = document.querySelector(".info-projeto3");
+	proj.classList.toggle("ativo");
+}
+function proj_out3()
+{
+	var proj = document.querySelector(".info-projeto3");
+	proj.classList.toggle("ativo");
+}
+
+function proj_enter4()
+{
+	var proj = document.querySelector(".info-projeto4");
+	proj.classList.toggle("ativo");
+}
+function proj_out4()
+{
+	var proj = document.querySelector(".info-projeto4");
+	proj.classList.toggle("ativo");
+}
+
+function proj_enter5()
+{
+	var proj = document.querySelector(".info-projeto5");
+	proj.classList.toggle("ativo");
+}
+function proj_out5()
+{
+	var proj = document.querySelector(".info-projeto5");
+	proj.classList.toggle("ativo");
+}
+
+function proj_enter6()
+{
+	var proj = document.querySelector(".info-projeto6");
+	proj.classList.toggle("ativo");
+}
+function proj_out6()
+{
+	var proj = document.querySelector(".info-projeto6");
+	proj.classList.toggle("ativo");
+}
